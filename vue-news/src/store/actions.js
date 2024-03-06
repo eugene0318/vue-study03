ec4d7bc7ef2d7db1d59c459b6702a4fcd64b6bc6
eugene <nttott@gmail.com>
2024-03-06 15:45:42 +0900

    'refactoring'

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -7,39 +7,72 @@ import {
   fetchList,
 } from "../api/index.js";
 export default {
-  FETCH_NEWS(context) {
-    fetchNewsList()
-      .then((response) => {
-        context.commit("SET_NEWS", response.data);
-        return response;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  //promise
+  // FETCH_NEWS(context) {
+  //   fetchNewsList()
+  //     .then((response) => {
+  //       context.commit("SET_NEWS", response.data);
+  //       return response;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+  //async
+  async FETCH_NEWS(context) {
+    const response = await fetchNewsList();
+    context.commit("SET_NEWS", response.data);
+    return response;
   },
-  FETCH_JOBS({ commit }) {
-    fetchJobsList()
-      .then(({ data }) => {
-        commit("SET_JOBS", data);
-      })
-      .catch();
+
+  // FETCH_JOBS({ commit }) {
+  //   fetchJobsList()
+  //     .then(({ data }) => {
+  //       commit("SET_JOBS", data);
+  //     })
+  //     .catch();
+  // },
+
+  async FETCH_JOBS({ commit }) {
+    try {
+      const response = await fetchJobsList();
+      context.commit("SET_JOBS", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_ASK({ commit }) {
-    fetchAskList()
-      .then(({ data }) => {
-        commit("SET_ASK", data);
-      })
-      .catch();
+
+  // FETCH_ASK({ commit }) {
+  //   fetchAskList()
+  //     .then(({ data }) => {
+  //       commit("SET_ASK", data);
+  //     })
+  //     .catch();
+  // },
+
+  async FETCH_ASK({ commit }) {
+    const response = await fetchAskList();
+    context.commit("SET_ASK", response.data);
+    return response;
   },
-  FETCH_USER({ commit }) {
-    fetchUserInfo()
-      .then(({ data }) => {
-        commit("SET_USER", data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+  // FETCH_USER({ commit }) {
+  //   fetchUserInfo()
+  //     .then(({ data }) => {
+  //       commit("SET_USER", data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+
+  async FETCH_USER({ commit }) {
+    const response = await fetchUserInfo();
+    context.commit("SET_USER", response.data);
+    return response;
   },
+
   FETCH_ITEM({ commit }, id) {
     fetchCommentItem(id)
       .then(({ data }) => {
@@ -49,14 +82,24 @@ export default {
         console.log(error);
       });
   },
-  FETCH_LISt({ commit }, pageName) {
-    return fetchList(pageName)
-      .then((response) => {
-        commit("SET_LIST", response.data);
-        return response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  // FETCH_LISt({ commit }, pageName) {
+  //   return fetchList(pageName)
+  //     .then((response) => {
+  //       commit("SET_LIST", response.data);
+  //       return response.data;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+
+  async FETCH_LISt({ commit }, pageName) {
+    try {
+      const response = await fetchList(pageName);
+      context.commit("SET_LIST", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
